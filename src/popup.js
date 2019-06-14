d94298f1698c4ede3e6417e6843a3eab8f2100cd
Ma'ayan Lab <maayanlab@alexanders-pro.anbg.mssm.edu>
2019-06-14 17:59:11 -0400

    integrated peershake webpage into biorxiv

--- src/popup.js
+++ src/popup.js
@@ -19,22 +19,30 @@
 
 //script to add chrome extension to bioRxiv page
 //we'll see how well this goes 
-function openFrame(frame){
+function openFrame(obj,lk,height){
     //var frame = document.querySelector(id);
-    frame.style.display="block";
-    frame.src=link1;
-    frame.setAttribute('width', '100%');
-    frame.setAttribute('height', '100%');
-    frame.style.visibility="visible";
+    obj.style.display="block";
+    obj.src=lk;
+    obj.setAttribute('width', '100%');
+    obj.setAttribute('height', height);
+    obj.style.visibility="visible";
     document.getElementById("panels-ajax-tab-container-highwire_article_tabs").style.display = 'none';
+    document.getElementsByClassName('active')[0].classList.remove('active');
+}
+function closeFrame(obj){
+    obj.style.display = 'none';
+    obj.setAttribute('width', '0px');
+    obj.setAttribute('height','0px');
 
 }
+
+
 //////////////////////////
 //necessary constants 
 var title=document.title
 var url = document.URL
-var link1 = 'https://peershake.cloud:35729/chromeExtensionBase/?title=' + encodeURIComponent(title) + "&url=" + encodeURIComponent(url);
-var link2 = 'http://127.0.0.1:35729/comment/' + encodeURIComponent(title);
+var link1 = 'https://127.0.0.1:35729/chromeExtensionBase/?title=' + encodeURIComponent(title) + "&url=" + encodeURIComponent(url);
+var link2 = 'https://127.0.0.1:35729/comment/' + encodeURIComponent(title);
 //////////////////////////
 
 //true code
@@ -57,8 +65,8 @@ a1.textContent = "Submit a Review";
 
 // a1.setAttribute('target', "_blank") //necessary to open link in new tab 
 
-// var a2 = document.createElement('a');
-// a2.textContent = "Reviews";
+var a2 = document.createElement('a');
+a2.textContent = "Reviews";
 // a2.setAttribute('href', link2);
 // a2.setAttribute('target', "_blank")
 
@@ -78,9 +86,13 @@ a1.style = "cursor: pointer;";
 // a1.appendChild(newDiv);
 // a1.appendChild(frame);
 
-// a2.className="panels-ajax-tab-tab panels-ajax-tabs-once-processed hw-panels-ajax-tabs-once-processed";
-// a2.style = "cursor: pointer;";
+a2.className="panels-ajax-tab-tab panels-ajax-tabs-once-processed hw-panels-ajax-tabs-once-processed";
+a2.style = "cursor: pointer;";
+
 a1.setAttribute('href', '#');
+a2.setAttribute('href','##');
+
+//creates submit review iframe and adds it to tab
 var frame = document.createElement('iframe');
 frame.id = "#subRev";
 frame.style.display=null;
@@ -88,12 +100,46 @@ frame.setAttribute('width', '0');
 frame.setAttribute('height', '0');
 frame.style.visibility="hidden";
 a1.addEventListener("click", function(){
-    openFrame(frame)
+    openFrame(frame,link1,'1000px')
+    closeFrame(frame2);
+    listAdd.className = "active"
+}  
+);
+// a1.addEventListener("click", function(){
+//     listAdd.className = "active"
+// } 
+// );
+
+//creates review comments iframe and adds it to tab 
+var frame2 = document.createElement('iframe');
+frame2.id = "#revCom";
+frame2.style.display=null;
+frame2.setAttribute('width', '0');
+frame2.setAttribute('height', '0');
+frame2.style.visibility="hidden";
+a2.addEventListener("click", function(){
+    openFrame(frame2,link2, '500px');
+    closeFrame(frame);
+    listAdd2.className = "active"
 } 
 );
+// a2.addEventListener("click", function(){
+//     listAdd2.className = "active"
+// } 
+// );
+
+
 // adds href links to proper list 
 listAdd.appendChild(a1);
 listAdd.appendChild(frame);
-// listAdd2.appendChild(a2);
-// refrenceNode.before(listAdd2);
+listAdd2.appendChild(a2);
+listAdd2.appendChild(frame2);
+refrenceNode.before(listAdd2);
 refrenceNode.before(listAdd);
+// window.location.href="";
+// window.location.reload(true);
+abMetList.getElementsByClassName('first')[0].addEventListener('click',function(){
+    closeFrame(frame);
+    closeFrame(frame2);
+    abMetList.style.display='block'
+});
